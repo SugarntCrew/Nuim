e08cf8bd66ad4c54b20d5d49f4e3b31c0a5cbf2e
MrMadera <90923803+MrMadera@users.noreply.github.com>
2026-07-08 23:43:19 +0100

    expanded some ui components

--- source/ui/header/Header.hx
+++ source/ui/header/Header.hx
@@ -12,8 +12,9 @@ class Header extends FlxSpriteGroup
         headerBackground.makeGraphic(width, height, 0xFFC9C9C9);
         add(headerBackground);
 
-        userAccountUI = new UserAccountUI();
+        userAccountUI = new UserAccountUI(0, 0, height, height); // double height because we want a square with the same proportions as the bg height
         userAccountUI.x = FlxG.width - userAccountUI.width;
+        userAccountUI.y = headerBackground.height / 2 - userAccountUI.height / 2;
         add(userAccountUI);
     }
 }
--- source/ui/header/UserAccountUI.hx
+++ source/ui/header/UserAccountUI.hx
@@ -2,6 +2,15 @@ package ui.header;
 
 class UserAccountUI extends FlxSprite implements IClickable
 {
+    public var scaleOffset:Float = 0.85;
+    public function new(x:Float = 0, y:Float = 0, width:Int, height:Int)
+    {
+        super(x, y);
+
+        makeGraphic(width, height, 0xFF000000); // temp !!
+        setGraphicSize(width * scaleOffset, height * scaleOffset);
+    }
+    
     public function onClick()
     {
         
