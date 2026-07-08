aa70b1ae0d6bef4316e40d0103a31067d9d252ab
MrMadera <90923803+MrMadera@users.noreply.github.com>
2026-07-08 23:06:09 +0100

    added game properties to main

--- source/Main.hx
+++ source/Main.hx
@@ -2,9 +2,19 @@ package;
 
 class Main extends Sprite
 {
+    public var game = {
+        width: 1920,
+        height: 1080,
+        initialState: MainState,
+        fps: 60,
+        skipSplash: true
+    }
+
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, MainState));
+        
+        var game = new FlxGame(game.width, game.height, game.initialState, game.fps, game.fps, game.skipSplash);
+		addChild(game);
 	}
 }
